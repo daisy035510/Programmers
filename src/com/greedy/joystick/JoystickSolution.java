c839248f76534ab9c21b90575283dd6e0302de97
daisy035510 <43841944+daisy035510@users.noreply.github.com>
2023-03-02 17:29:21 +0900

    init commit

---
+++ src/com/greedy/joystick/JoystickSolution.java
@@ -0,0 +1,39 @@
+package com.greedy.joystick;
+
+class JoystickSolution {
+
+    public static void main(String[] args) {
+
+        // JEROEN
+        // JAN
+        int rlst = solution("JEROEN");
+        System.out.println("결과값 :" + rlst);
+    }
+    public static int solution(String name) {
+
+        int upDown  = 0;
+        int len     = name.length();
+        int move    = len - 1; // 오른쪽으로 쭉 간 회수
+
+        for(int i = 0; i < len; i++) {
+
+            upDown += Math.min(name.charAt(i) - 'A', 26 - (name.charAt(i) - 'A'));
+            int nextIndex = i + 1;
+            // 다음 단어가 A이고, 단어가 끝나기 전까지 nextIndex 증가
+            while(nextIndex < len && name.charAt(nextIndex) == 'A') {
+                nextIndex++;
+            }
+            //min=Math.min(min,i+length-next+Math.min(i,length-next));
+            move = Math.min(move, i * 2 + (name.length() - nextIndex));// 오른쪽으로 갔다 다시 왼쪽으로 꺾기
+            move = Math.min(move, i + (name.length() - nextIndex) * 2);// 왼쪽으로 갔다 다시 오른쪽으로 꺾기
+        }
+        System.out.println("upDown : " + upDown);
+        System.out.println("move : " + move);
+
+        int answer = upDown + move;
+        return answer;
+    }
+
+
+}
+
